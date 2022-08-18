85d281c2e6c8e935d8f38a880c2be8e25b7da9f9
matiasbrandan <106163161+matiasbrandan@users.noreply.github.com>
2022-08-18 05:28:44 -0300

    segunda preentrega

--- js/index.js
+++ js/index.js
@@ -1,62 +1,130 @@
-// OBJETOS
+//VARIABLES
 
-class datosDelSolicitante {
-    constructor (nombre,apellido,monto,cuotas){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.monto = monto;
-        this.cuotas = cuotas;
+let inputName = document.getElementById ("userName");
+let inputSurname = document.getElementById("userSurname");
+let inputMonto = document.getElementById ("userMonto");
+let cuotas = document.getElementById ("userCuotas");
+let btnCalcular = document.getElementById("btnCalcular");
+let nombre ;
+let surname ;
+let monto ;
+let cuotaSelecionada;
+inputName.addEventListener("keydown", event => {
+        if(event.key == "Enter"){ //Tecla enter
+            console.log('hola ney');
+        }
     }
+);
+//OBJETO
 
-}
+// class datosDelSolicitante {
+//     constructor (nombre,apellido,monto,cuotas){
+//         this.nombre = nombre;
+//         this.apellido = apellido;
+//         this.monto = monto;
+//         this.cuotas = cuotas;
+//     }
+
+// }
 
 // ARRAY
 
-const solicitante = [];
+
+// const solicitante = [];
+
+const opcionesDeCuotas = [6, 12, 18, 24, 30, 36];
 
 //funcuiones
 
-function nuevoSolicitante () {
-    let nombre = prompt("ingrese su nombre:");
-    let apellido = prompt("Ingrese su apellido:");
-    let monto = parseInt(prompt("Ingresa el monto:"));
-    alert ("elija la cantidad de cuotas de las siguentes opciones")
-    let cuotas = prompt ("6 - 12 - 18 - 24 -30 -36");
-        solicitante.push(new datosDelSolicitante(nombre, apellido, monto, cuotas))
-        console.table(solicitante)
-}
-nuevoSolicitante ()
+function mostrar () {
 
-for (const calcularCuota of solicitante) {
-    let cuotas = calcularCuota.cuotas;
-    let monto = calcularCuota.monto;
-    switch (cuotas) {
-        case "6":
-            let primera = monto + (monto * 0.05);
-            alert ("su monto a abonar es " + primera / 6 + "por mes");
-            break;
-        case "12":
-            let segunda = monto + (monto * 0.1);
-            alert ("su monto a abonar es " + segunda / 12 + "por mes");
-            break;
-        case "18":
-            let tercera = monto + (monto * 0.25);
-            alert ("su monto a abonar es " + tercera / 18 + "por mes");
-            break;
-        case "24":
-            let cuarta = monto + (monto * 0.35);
-            alert ("su monto a abonar es " + cuarta / 24 + "por mes");
-            break;
-        case "30":
-            let quinta = monto + (monto * 0.4 );
-            alert ("su monto a abonar es " + quinta / 30 + "por mes");
-        case "36":
-            let sexta = monto + (monto * 0.5 );
-            alert ("su monto a abonar es " + sexta / 36 + "por mes");
-        default:
-            alert ("no es una cantidad de cuotas validas");
+    inputName.onchange = () => {
+        nombre=inputName.value;
+    }
+    inputSurname.onchange = () => {
+        surname=inputSurname.value;
+    }
+    inputMonto.onchange = () => {
+        monto=parseInt (inputMonto.value);
+    }
+    cuotas.onchange = () => {
+        cuotaSelecionada=cuotas.value;
+    }
+    
+}
+
+function interes (){
+    for (porMes of opcionesDeCuotas){
+        switch (cuotaSelecionada) {
+                    case "6":
+                        let primera = monto + (monto * 0.05);
+                        return primera;
+                    case "12":
+                        let segunda = monto + (monto * 0.1);
+                        return segunda;
+                    case "18":
+                        let tercera = monto + (monto * 0.25);
+                        return tercera;
+                    case "24":
+                        let cuarta = monto + (monto * 0.35);
+                        return cuarta;
+                    case "30":
+                        let quinta = monto + (monto * 0.4 );
+                        return quinta;
+                    case "36":
+                        let sexta = monto + (monto * 0.5 );
+                        return sexta;
+                    default:
+                        console.log ("no es una cantidad de cuotas validas");
+                        break
+                }
     }
 }
 
 
 
+
+function mostrarDatos() {
+        let body = document.body
+        let seccion = document.getElementById("mostrar")
+        body.addEventListener("keydown", e => {
+                if (e.key == "Enter") {
+                    seccion.innerHTML+= `<p>nombre:${nombre}</p>
+                                    <p>apellido:${surname}</p>
+                                    <p>monto:${monto}</p>
+                                    <p>cuotas${cuotaSelecionada}</p>
+                                    <p>por mes:${interes(cuotaSelecionada)}`
+                }
+            }
+        )
+        btnCalcular.onclick = () => {
+            seccion.innerHTML+= `<p>nombre:${nombre}</p>
+                                    <p>apellido:${surname}</p>
+                                    <p>monto:${monto}</p>
+                                    <p>cuotas${cuotaSelecionada}</p>
+                                    <p>por mes:${interes(cuotaSelecionada)}`
+        }
+}
+
+//Ciclos
+
+for (opciones of opcionesDeCuotas) {
+    let option = document.createElement("option");
+    option.innerHTML=opciones;
+    cuotas.appendChild(option);
+}
+
+
+
+
+// for (const calcularCuota of solicitante) {
+//     let cuotas = calcularCuota.cuotas;
+//     let monto = calcularCuota.monto;
+//     
+// }
+
+
+
+//llamar funciones
+mostrar ();
+mostrarDatos();
